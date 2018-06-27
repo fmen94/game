5dfbfa08368f9ace39a1b231bda052dccac944e5
Fernando <fmen9400@gmail.com.com>
2018-06-27 15:04:12 -0500

    terminado

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-//constants
+
+//constantes
 cartas=[
     {valor: 1, imagen: "images/imagenes de cartas/A corazones .png"},
     {valor: 1, imagen: "images/imagenes de cartas/A diamantes.png"},
@@ -48,6 +49,14 @@ cartas=[
     var cartasUser=[];
     var cartasComp=[];
     var fondoImg="images/Mesa_poker.png";
+    var cartaBack="images/carta.png";
+    var mas=false;
+    var mas1=false;
+    var mas2=false;
+    var turno=true;
+    var masCom=false;
+    var masCom1=false;
+    var ganador=0;
 
     
 
@@ -71,11 +80,11 @@ cartas=[
         }
     }
     class Cartas{
-        constructor(x,y,image){
+        constructor(x,y,image=cartaBack){
             this.x=x;
             this.y=y;
-            this.width=60;
-            this.height=80;
+            this.width=108;
+            this.height=144;
             this.image =new Image();
             this.image.src= image;
             this.image.onload=function(){
@@ -86,45 +95,89 @@ cartas=[
             ctx.drawImage(this.image, this.x,this.y,this.width,this.height); 
 
         }
+        //si es verdadero direc va hacia el usuario
+        mover(x,y,direc){
+
+            if(direc){
+                if(this.y<454){this.y+=y;
+                this.x-=x;}
+            }else{
+                if(this.y>50){this.y-=y;
+                    this.x-=x;}
+            }
+            
+        }
 
 
     }              
 //instances
-    fondo= new Fondo();
-    carta= new Cartas(50,50,cartas[0].imagen);
+    darDosCartas();
+    var  fondo= new Fondo();
+    var carta= new Cartas(50,50,cartas[0].imagen);
+    var carta1= new Cartas(584,267,cartasUser[0].imagen);
+    var carta2= new Cartas(584,267,cartasUser[1].imagen);
+    var carta3= new Cartas(584,267);
+    var carta4= new Cartas(584,267);
+    var cartaAdi=new Cartas(584,267);
+    var cartaAdi1=new Cartas(584,267);
+    var cartaAdi2=new Cartas(584,267);
+    var cartaCom3=new Cartas(524,267);
+    var cartaCom4=new Cartas(584,267);
+    
+
 //*****************mainFunctions***************************
 function update(){
     frames++;
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    
     fondo.draw();
-    reparte();
+    carta.draw();
+    carta1.draw();
+    carta1.mover(1.2,2,true);
+    carta2.draw();
+    carta2.mover(0,2,true);
+    carta3.draw();
+    carta3.mover(1.05,2,false);
+    carta4.draw();
+    carta4.mover(0,2,false);
+    puntuacion()
+    if(mas){cartaAdi.draw();
+    cartaAdi.mover(-1.2,2,true);}
+    if(mas1){cartaAdi1.draw();
+    cartaAdi1.mover(-2.4,2,true);}
+    if(mas2){cartaAdi2.draw();
+    cartaAdi2.mover(2.4,2,true);}
+    if(tePasaste(cartasUser)==false||ganador==2){
+        turno=false;
+        ctx.font = '100px Paytone One';
+        ctx.fillStyle = 'red';
+        ctx.fillText('GAME OVER', 350, 350);
+    }
+    if(masCom){cartaCom3.draw();
+        cartaCom3.mover(-1.6,2,false);}
+    if(masCom1){cartaCom4.draw();
+        cartaCom4.mover(-2.1,2,false)}
+    if(turno==false){
+        if(tePasaste(cartasComp)==false&&tePasaste(cartasUser)||ganador==1){
+            ctx.font = '100px Paytone One';
+            ctx.fillStyle = 'blue';
+            ctx.fillText('WIN!', 500, 350);
+        }
+    }
+
 }
 
 function start(){
-    interval = setInterval(update, 1000/180);
-
+    interval = setInterval(update, 1000/60);
 }
 //*****************aux functions*************************
 function creaCartas(){
-
-
-
-
-
-
+    mas=true;
 }
 
-function reparte(){
-darDosCartas();
-var x=584;
-var y=267;
-carta1= new Cartas(x,y,cartasUser[0].imagen);
 
 
-}
 
-//entrega un numero random dentro del arreglo
+//entrega un arreglo random dentro del arreglo
 function cartaAzar(cartas){
     var numCar=Math.floor(Math.random()*cartas.length);
     var cartaSelec=cartas[numCar];
@@ -135,7 +188,6 @@ function cartaAzar(cartas){
 }
 //dar las dos cartas iniciales a los jugadores 
 function darDosCartas(){
-    if(cartasUser.length>=2)return
     //entrega 2 cartas al usuario
     var carta1=cartaAzar(cartas);
     cartasUser[0]=carta1;
@@ -147,11 +199,17 @@ function darDosCartas(){
     var carta4=cartaAzar(cartas);
     cartasComp[1]=carta4;
 }
+//funcion que muestre los puntos del usuario
+function puntuacion(){
+    
+    ctx.font = '100px Paytone One';
+        ctx.fillStyle = 'white';
+        ctx.fillText(contadora(cartasUser), 940, 580);  
+}
 //recibe a quien se le añaden cartas
 function addCarta(array){
-    if (tePasaste(array)){
     var cartaAdd=cartaAzar(cartas);
-    array.push(cartaAdd);}
+    array.push(cartaAdd);
 }
 //esta funcion se actualiza cada que el usuario o computadora agrega una carta
 function contadora(arrayDeCartas){
@@ -164,11 +222,10 @@ function contadora(arrayDeCartas){
 }
 //si es verdadero tiene menos de 7.5 es verdadero
 function tePasaste(array){
-    if(contadora(array)<7.5) return true;
+    if(contadora(array)<=7.5) return true;
     return false;
-
 }
-
+//ventaja de la computadora final
 function ventajaCompu(){
     var puntosCom=contadora(cartasComp);
     if(puntosCom<=7){puntosCom+=.5};
@@ -178,7 +235,7 @@ function ventajaCompu(){
 //si es falso gana la compu
 //si es verdadero gana el usuario
 function saberQuienGana(){
-    if (ventajaCompu()>contadora(cartasUser)){
+    if (ventajaCompu()>contadora(cartasUser)&&ventajaCompu()<=7.5){
         console.log("perdiste");
         return false};
     if (ventajaCompu()<contadora(cartasUser)){
@@ -187,12 +244,65 @@ function saberQuienGana(){
         console.log("empate");
     return "empate";
 }
+//la computadora roba carta si la suma es menor a 5
 function compuRoba(){
     if(contadora(cartasComp)<=5){addCarta(cartasComp)}
 }
 
 
-//listeners
+
+//******************************listeners********************************
+$('#inicio').click(function(){
+    start();
 
 
-start();
+});
+$('#otraCarta').click(function(){
+    if(turno){
+        console.log("agragaste una carta");
+        addCarta(cartasUser);
+        if(mas1){mas2=true};
+        if (mas){mas1=true};
+        if(mas==false){creaCartas();}
+    }
+    
+});
+$('#canvas').click(function(){
+    
+        if(mas){
+            cartaAdi.image.src=cartasUser[2].imagen;
+        }
+        if(mas1){
+            cartaAdi1.image.src=cartasUser[3].imagen;
+        }
+        if(mas2){
+            cartaAdi2.image.src=cartasUser[4].imagen;
+        }
+    
+    
+
+});
+$('#terminar').click(function(){
+    turno=false;
+    carta3.image.src=cartasComp[0].imagen;
+    carta4.image.src=cartasComp[1].imagen;
+    compuRoba();
+    if(cartasComp.length==3){
+        masCom=true;
+        cartaCom3.image.src=cartasComp[2].imagen;
+    }
+    compuRoba();
+    if(cartasComp.length==4){
+        masCom1=true;
+        cartaCom4.image.src=cartasComp[3].imagen;
+    }
+    if(saberQuienGana()){
+        ganador=1;
+    }
+    if(saberQuienGana()==false){
+        ganador=2
+    }
+})
+$('#restart').click(function(){
+    location.reload();
+})
